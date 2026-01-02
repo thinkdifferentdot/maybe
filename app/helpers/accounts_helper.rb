007b01cab4a05de38e012c01bdbb43cf06d6b786
thinkdifferentdot <andybewernick@me.com>
2026-01-01 18:17:10 -0700

    feat: add helper methods for account type and subtype options

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -3,4 +3,19 @@ module AccountsHelper
     content = capture(&block)
     render "accounts/summary_card", title: title, content: content
   end
+
+  def accountable_type_options
+    Accountable::TYPES.map do |type|
+      [ type.constantize.display_name, type ]
+    end
+  end
+
+  def subtype_options_for(accountable_type)
+    return [] unless accountable_type.present?
+
+    klass = accountable_type.constantize
+    return [] unless klass.const_defined?(:SUBTYPES)
+
+    klass::SUBTYPES.map { |key, labels| [ labels[:long], key ] }
+  end
 end
